46f29c30287433eba4b6d4f698ff690ae5197f68
Brian B <brianb@MacBook-Pro-4.local>
2019-02-26 19:01:03 -0500

    Adds ability to see simple 'home' template from browser when running app

--- src/main/java/com/detroitlabs/photolibrary/controllers/PhotoController.java
+++ src/main/java/com/detroitlabs/photolibrary/controllers/PhotoController.java
@@ -1,8 +1,15 @@
 package com.detroitlabs.photolibrary.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PhotoController {
+
+    @RequestMapping("/")
+    public String displayHome(){
+        return "home";
+    }
+
 
 }
